2188e868a1230d0fdc4c5330e28891fa0f48428b
alonjoe <alonjoe@gmail.com>
2021-11-23 18:40:05 +0900

    [21.11.23] # js기본구조

---
+++ 1. core.js/1. 변수/3. quiz.js
@@ -0,0 +1,16 @@
+
+// 1번 문제
+// 현재 우리가 살고있는 행성(planet)의 이름을 값으로 가진 변수를 만들어보세요. 변수 이름은 어떻게 지어야 할까요?
+
+var admin, myName;
+myName = 'John';
+admin = myName;
+console.log(admin);
+
+// 2번 문제 웹사이트를 개발 중이라고 가정하고, 현재 접속 중인 사용자(user)의 이름(name)을 저장하는 변수를 만들어보세요. 변수 이름은 어떻게 지어야 할까요?
+var ourLivingPlanet = '지구';
+console.log(ourLivingPlanet);
+
+var correntLoginUserName = '';
+var conntectedUserName = '';
+
