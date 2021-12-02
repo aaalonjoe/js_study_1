6266a435ea9d6a7365009ad0e00b9d8d823a3561
alonjoe <alonjoe@gmail.com>
2021-12-02 18:53:08 +0900

    [21.12.02] 배열 기초

---
+++ 1. core.js/5. 타입변환/1. 암묵적 형변환.js
@@ -0,0 +1,13 @@
+
+
+var ex = 10 + '20';
+console.log(ex);
+
+var num = 200 + '';
+console.log(typeof num);
+
+var ex2 = '100' - '50'; 
+console.log(ex2);
+
+var ex3 = '100' * '55';
+console.log(ex3);
