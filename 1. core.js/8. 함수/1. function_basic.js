9766977b3c5d8c4217c977466b68166653dd299e
alonjoe <alonjoe@gmail.com>
2021-12-07 18:50:50 +0900

    [21.12.07] 함수

---
+++ 1. core.js/8. 함수/1. function_basic.js
@@ -0,0 +1,25 @@
+
+
+
+// console.log('김철수님 안녕하세요!');
+// console.log('오늘도 좋은하루되세요!');
+
+// 함수 정의
+function sayHello(userName) {
+    console.log(`${userName}님 안녕하세요! `);
+    console.log(`오늘도 즐거운 하루되세요~~~`);
+    console.log(`${userName}님 짱짱짱!!!`);
+    console.log('=============================');
+}
+
+// 100줄~~~~
+
+sayHello('박영희');
+
+// 1000줄~~~~~
+
+sayHello('김철수');
+
+// 5만줄~~~~~~~
+
+sayHello('둘리');
